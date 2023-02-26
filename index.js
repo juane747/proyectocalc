ea7125d7c37efe3fe16a6ef7fcdc254c7a91a368
Emilio Garcia <juane747@hotmail.com>
2023-02-26 14:47:38 -0600

    first comit

---
+++ index.js
@@ -0,0 +1,30 @@
+//var Mousetrap= require('mousetrap');
+
+var botonUno = document.getElementById('botonUno')//conseguimos la informacion de botonUno y la guardamos en variable botoUno
+var actual = document.getElementById('Actual')
+var resultado=document.getElementById('Resultado')
+var botonDos = document.getElementById('botondos')
+
+var botonSuma= document.getElementById('botonsuma')
+
+botonUno.addEventListener('click',clickUno)//escuchamos al hacer clic en botonUno
+botonDos.addEventListener('click',clickdos)
+
+botonSuma.addEventListener('click',clicksuma)
+
+//Mousetrap.bind('1',clickUno)
+
+//funcion definida al hacer click en botonUno
+function clickUno(){
+    actual.innerHTML+='1'// con esto agregamos un uno al momento de dar clic a la etiqueta actual de index html y lo guarda como string
+}
+function clickdos(){
+    actual.innerHTML+='2'
+}
+
+function clicksuma(){
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    resultado.innerHTML= valorActual + valorResultado
+    actual.innerHTML='0'
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow}=require('electron');// nos permite llamar estas funciones como importar clases en java
+
+function createWindow(){//indicamos que estamos creando una ventana
+    const ventana = new BrowserWindow({//definimos la nueva ventana
+        width:300,//aca definimos alto y ancho
+        height:300
+        //webPreferences:{//para darnos acceso
+          // nodeIntegration:true,
+            //contextIsolation:false
+       // }
+    });
+    ventana.loadFile('index.html')//cargamos nuestra ventana
+}
+
+app.whenReady().then(createWindow)//inicializamos nuestra ventana
